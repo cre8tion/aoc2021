b030b76ce0b660b4a257d6bd8a133225b3b201bf
Cre8tion <ljl1997@gmail.com>
2021-12-05 01:09:36 +0800

    day3 part2

--- src/day3.rs
+++ src/day3.rs
@@ -11,7 +11,7 @@ impl std::fmt::Display for Binary {
     fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
         match *self {
             Binary::Zero => write!(fmt, "0"),
-            Binary::One => write!(fmt, "1")
+            Binary::One => write!(fmt, "1"),
         }
     }
 }
@@ -51,13 +51,41 @@ pub fn part1(input: &[Vec<Binary>]) -> usize {
         .map(|val| if val > &0 { Binary::One } else { Binary::Zero })
         .collect::<Vec<Binary>>();
 
-    let epsilon_rate_vec = gamma_rate_vec.iter().map(|b| match b{
-        Binary::Zero => Binary::One,
-        Binary::One => Binary::Zero
-    }).collect::<Vec<Binary>>();
+    let epsilon_rate_vec = gamma_rate_vec
+        .iter()
+        .map(|b| match b {
+            Binary::Zero => Binary::One,
+            Binary::One => Binary::Zero,
+        })
+        .collect::<Vec<Binary>>();
 
     let gamma_rate = usize::from_str_radix(&join(gamma_rate_vec, ""), 2).unwrap();
     let epsilon_rate = usize::from_str_radix(&join(epsilon_rate_vec, ""), 2).unwrap();
 
     gamma_rate * epsilon_rate
+}
+
+#[aoc(day3, part2)]
+pub fn part2(input: &[Vec<Binary>]) -> u32 {
+    let input_vec = input.iter().map(|b| join(b, "")).collect::<Vec<String>>();
+    part2_solver(&input_vec, true) * part2_solver(&input_vec, false)
+}
+
+fn part2_solver(lines: &Vec<String>, most: bool) -> u32 {
+    let mut pos = 0;
+    let mut values = lines.clone();
+    while values.len() > 1 {
+        let ones_count = values
+            .iter()
+            .map(|x| x.chars().nth(pos).unwrap())
+            .filter(|x| *x == '1')
+            .count();
+        if ones_count >= (values.len() - ones_count) {
+            values.retain(|x| x.chars().nth(pos).unwrap() == if most { '1' } else { '0' })
+        } else {
+            values.retain(|x| x.chars().nth(pos).unwrap() == if most { '0' } else { '1' })
+        }
+        pos += 1
+    }
+    u32::from_str_radix(&values[0], 2).unwrap()
 }
